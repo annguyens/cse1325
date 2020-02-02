1d8611af57929bb5ab1a3ff6535127b3b1b684db
An Nguyen <an.nguyen8@mavs.uta.edu>
2020-02-02 00:36:23 -0600

    P02 color.cpp 2nd

--- P02/full_credit/color.cpp
+++ P02/full_credit/color.cpp
@@ -10,12 +10,12 @@ Color::Color(int red, int green, int blue)
 
 std::string Color::to_string()
 {
-	std::string format = '(' + std::to_string(_red) + ',' + 		  std::to_string(_green) + ',' + std::to_string(_blue) + ')';
+	std::string format = "(" + std::to_string(_red) + "," +std::to_string(_green) + "," + std::to_string(_blue) + ")";
 	return format;
 }
 
 std::string Color::colorize(std::string text)
 {
-	std::string disp =  "\033[38;2;" + std::to_string(_red) + ';' + std::to_string(_green) + ';' +std::to_string(_blue) + "177m" + text + "\033[0m";
+	std::string disp = "\033[38;2;" + std::to_string(_red) + ";" + std::to_string(_green) + ";" +std::to_string(_blue) + ";177m" + text + "\033[0m";
 	return disp;
 }

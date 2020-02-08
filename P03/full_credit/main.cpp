6af8b4d8cc2a4378bad8276419317f499a61fe88
An Nguyen <an.nguyen8@mavs.uta.edu>
2020-02-08 15:21:23 -0600

    P03

--- P03/full_credit/main.cpp
+++ P03/full_credit/main.cpp
@@ -5,7 +5,7 @@ int main() {
 	Color purple{100,0,110}, gold{212,180,55}, orange{255,165,0};
 	Color reset{};
 	
-    	std::cout << purple << "Purple " << gold << "Gold " << orange << "Orange " 		<< reset << std::endl;
+    	std::cout << purple << "Purple " << gold << "Gold " << orange << "Orange " <<reset << std::endl;
 
 	Color color{0,0,0};
 	std::cout << "\nEnter color as (red, green, blue): ";

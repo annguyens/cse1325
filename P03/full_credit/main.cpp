c2e1830c207591889b3475aab4b89a67e6e5a0b2
An Nguyen <an.nguyen8@mavs.uta.edu>
2020-02-08 15:18:47 -0600

    P03 f_c fc

---
+++ P03/full_credit/main.cpp
@@ -0,0 +1,14 @@
+#include "color.h"
+#include <iostream>
+
+int main() {
+	Color purple{100,0,110}, gold{212,180,55}, orange{255,165,0};
+	Color reset{};
+	
+    	std::cout << purple << "Purple " << gold << "Gold " << orange << "Orange " 		<< reset << std::endl;
+
+	Color color{0,0,0};
+	std::cout << "\nEnter color as (red, green, blue): ";
+	std::cin >> color;
+	std::cout << color << color.to_string() << std::endl;
+}
